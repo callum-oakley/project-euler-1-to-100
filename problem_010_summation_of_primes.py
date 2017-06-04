e0b69ef2c95d3d31edef9cd1b53a1ac9ac19ec6f
Callum Oakley <callum@pusher.com>
2017-06-04 13:03:09 +0100

    010

---
+++ problem_010_summation_of_primes.py
@@ -0,0 +1,8 @@
+def primes(n):
+    candidates = set(range(2, n))
+    for i in range(2, n):
+        if i in candidates:
+            yield i
+            candidates -= set(range(i, n, i))
+
+# sum(primes(2 * 10 ** 6)) == 142913828922
