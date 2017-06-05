1bed62d00110b0fc566c42536a146cf4193a01c5
Callum Oakley <callum@pusher.com>
2017-06-05 22:14:07 +0100

    cleanup

--- problem_024_lexicographic_permutations.py
+++ problem_024_lexicographic_permutations.py
@@ -4,10 +4,7 @@ def permute(xs, n):
     if len(xs) == 1:
         return xs
     granularity = factorial(len(xs) - 1)
-    index = n // granularity
-    skipped = index * granularity
-    return xs[index:index + 1] + permute(
-        xs[:index] + xs[index + 1:], n - skipped
-    )
+    i = n // granularity
+    return xs[i] + permute(xs[:i] + xs[i + 1:], n - i * granularity)
 
 print(permute("0123456789", 10 ** 6 - 1)) # 2783915460
