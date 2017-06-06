fe4189aa7d4119eb75bbeca597d407f57d4e4a8f
Callum Oakley <callum@pusher.com>
2017-06-06 22:43:58 +0100

    025

---
+++ problem_025_1000_digit_fibonacci_number.py
@@ -0,0 +1,15 @@
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def fibonacci(n):
+    if n <= 2:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+def naturals():
+    n = 0
+    while True:
+        yield n
+        n += 1
+
+print(next(i for i in naturals() if len(str(fibonacci(i))) >= 1000))
