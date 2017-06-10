5cb5f5bba34106bc03ae0b8db151f99afc43efbe
Callum Oakley <callum@pusher.com>
2017-06-10 01:08:48 +0100

    026

--- problem_026_reciprocal_cycles.py
+++ problem_026_reciprocal_cycles.py
@@ -1,8 +1,13 @@
 def reciprocalCycle(denominator):
     longDivision = [(0, 1)]
     while True:
-        longDivision.append(divmod(longDivision[-1][1] * 10, denominator))
-        print(longDivision)
-        input()
+        digit, remainder = divmod(longDivision[-1][1] * 10, denominator)
+        for i in range(len(longDivision)):
+            if longDivision[-1 - i] == (digit, remainder):
+                return [digit for digit, _ in longDivision[-1 - i:]]
+        longDivision.append((digit, remainder))
 
-print(reciprocalCycle(7))
+def reciprocalCycleLength(denominator):
+    return len(reciprocalCycle(denominator))
+
+print(max(range(1, 1000), key=reciprocalCycleLength)) # 983
