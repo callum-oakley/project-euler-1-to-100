898d640f6b4ecf13841c43104eaa8574e6de500a
Callum Oakley <callum@pusher.com>
2017-06-10 12:22:28 +0100

    028

---
+++ problem_028_number_spiral_diagonals.py
@@ -0,0 +1,9 @@
+def corners(start, jump):
+    return (start + i * jump for i in range(1, 5))
+
+def diagonals(sideLength):
+    yield 1
+    for ring in range((sideLength - 1) // 2):
+        yield from corners((2 * ring + 1) ** 2, 2 * (ring + 1))
+
+print(sum(n for n in diagonals(1001))) # 669171001
