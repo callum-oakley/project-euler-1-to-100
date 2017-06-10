7172651e27e8c00c788de540aff58049794841f0
Callum Oakley <callum@pusher.com>
2017-06-10 19:18:51 +0100

    is -> are

--- problem_032_pandigital_products.py
+++ problem_032_pandigital_products.py
@@ -1,7 +1,7 @@
 def isPandigital(s):
     return len({d for d in s} - {"0"}) == 9
 
-def isSufficientlySmall(a, b):
+def areSufficientlySmall(a, b):
     # If the expression for the product of a and b contains more than 9 digits,
     # then a and b can't possibly form a pandigital product.
     return len(str(a) + str(b) + str(a * b)) < 10
@@ -10,9 +10,9 @@ def pandigitalProducts():
     a, b = 1, 2
     # We assume throughout that a < b. If a and a + 1 are not sufficiently
     # small, then a, b won't be sufficiently small for any b.
-    while isSufficientlySmall(a, a + 1):
+    while areSufficientlySmall(a, a + 1):
         # Likewise, if a and b are not sufficently small, then a, b' won't be sufficiently small for any b' > b.
-        while isSufficientlySmall(a, b):
+        while areSufficientlySmall(a, b):
             if isPandigital(str(a) + str(b) + str(a * b)):
                 yield a * b
             b += 1
