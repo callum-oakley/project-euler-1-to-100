ff996906a8d67369995302e082fc1f5bbcdb5f33
Callum Oakley <callum@pusher.com>
2017-06-12 22:25:21 +0100

    refactor

--- problem_043_sub_string_divisibility.py
+++ problem_043_sub_string_divisibility.py
@@ -2,7 +2,7 @@ def isPandigitalish(s):
     return len({d for d in s}) == len(s)
 
 def assemble(slices):
-    return "".join(s[0] for s in slices) + slices[-1][1:]
+    return "".join(slice[0] for slice in slices) + slices[-1][1:]
 
 def isValid(slices):
     return all(
@@ -12,20 +12,17 @@ def isValid(slices):
 
 def substringDivisiblePandigitals():
     divisors = [1, 2, 3, 5, 7, 11, 13, 17]
-    # Since we know each 3 digit slice of the final solution is divisible by
-    # the above, we can search the space of possible slices, which turns out to
-    # be way faster than searching the space of 0-9-pandigitals.
-    slices = ["000" for _ in divisors]
-    head = len(slices) - 1
+    # Since we know each 3 digit slice is divisible by each of the above in
+    # turn, we can search the space of possible slices, which turns out to be
+    # way faster than searching the space of 0-9-pandigitals.
+    slices, head = ["000" for _ in divisors], len(divisors) - 1
     while head < len(slices):
         slices[head] = str(int(slices[head]) + divisors[head]).zfill(3)
         if len(slices[head]) > 3:
-            slices[head] = "000"
-            head += 1
+            slices[head], head = "000", head + 1
+        elif head == 0 and isValid(slices):
+            yield assemble(slices)
         elif isValid(slices[head:]):
-            if head == 0:
-                yield assemble(slices)
-            else:
-                head -= 1
+            head -= 1
 
 print(sum(int(s) for s in substringDivisiblePandigitals())) # 16695334890
