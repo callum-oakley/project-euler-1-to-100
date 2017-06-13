ce76bcc55e971e6b2a59600d24cdc54d91e330ef
Callum Oakley <callum@pusher.com>
2017-06-13 22:47:19 +0100

    044

--- problem_044_pentagon_numbers.py
+++ problem_044_pentagon_numbers.py
@@ -5,20 +5,20 @@ def p(n):
     return n * (3 * n - 1) // 2
 
 def pentagonalDifferences():
-    n, pentagonals, maxN, difference = 2, set(), 0, float("inf")
+    n, pentagonals, maxN, minDifference = 2, set(), 0, float("inf")
     # We can stop searching when the difference between each pentagonal number
     # is greater than the smallest difference we have found so far. The
     # solution as actually found very early, and most of the running time is in
     # exhausting all the rest of the possibilities...
-    while p(n) - p(n - 1) < difference:
+    while p(n) - p(n - 1) < minDifference:
         m = n - 1
-        while m > 0 and p(n) - p(m) < difference:
+        while m > 0 and p(n) - p(m) < minDifference:
             while p(maxN) < p(n) + p(m):
                 maxN += 1
                 pentagonals.add(p(maxN))
-            if p(n) + p(m) in pentagonals and p(n) - p(m) in pentagonals:
-                difference = p(n) - p(m)
-                yield difference
+            if p(n) - p(m) in pentagonals and p(n) + p(m) in pentagonals:
+                minDifference = p(n) - p(m)
+                yield minDifference
             m -= 1
         n += 1
 
