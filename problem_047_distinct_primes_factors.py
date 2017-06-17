970865166695c0c6e7207eb1bfebded06ea482c9
Callum Oakley <callum@pusher.com>
2017-06-17 16:56:54 +0100

    047

--- problem_047_distinct_primes_factors.py
+++ problem_047_distinct_primes_factors.py
@@ -7,11 +7,26 @@ def infRange(n=0, step=1):
 
 @lru_cache(maxsize=None)
 def distinctPrimeFactors(n):
-    if n == 1:
+    if n <= 1:
         return set()
     divisor = 2
     while n % divisor != 0:
         divisor += 1
     return {divisor} | distinctPrimeFactors(n // divisor)
 
-print(distinctPrimeFactors(644))
+def consecutiveRuns(ns):
+    run = []
+    for n in ns:
+        if len(run) == 0 or n == run[-1] + 1:
+            run.append(n)
+        else:
+            yield run
+            run = [n]
+    yield run
+
+print(next(
+    run[0] for run in consecutiveRuns(
+        n for n in infRange()
+        if len(distinctPrimeFactors(n)) == 4
+    ) if len(run) == 4
+)) # 134043
