306953379e362e899470b496624e05245e79f71b
Callum Oakley <callum@pusher.com>
2017-06-18 14:14:44 +0100

    neatening

--- problem_051_prime_digit_replacements.py
+++ problem_051_prime_digit_replacements.py
@@ -9,22 +9,25 @@ def genPrimes(n):
 
 @lru_cache(maxsize=None)
 def intToPattern(n):
+    # Each pattern is just n written in base 11
     if n == 0:
         return ""
     if n % 11 == 10:
         return intToPattern(n // 11) + "*"
     return intToPattern(n // 11) + str(n % 11)
 
+def expand(pattern):
+    return (
+        int(r) for r in (pattern.replace("*", str(d)) for d in range(10))
+        if len(r) == len(pattern)
+    )
+
 def findSmallestMember(matches):
     bound = 2
     while True:
         primes = set(genPrimes(10 ** bound))
         for pattern in (intToPattern(n) for n in range(1, 11 ** bound)):
-            primeReplacements = {
-                n for n in (
-                    int(pattern.replace("*", str(d))) for d in range(10)
-                ) if n in primes and len(str(n)) == len(pattern)
-            }
+            primeReplacements = {n for n in expand(pattern) if n in primes}
             if len(primeReplacements) == matches:
                 return min(primeReplacements)
         bound += 1
