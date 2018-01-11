52142583ea46c2db5e639af36542195352d347b8
Callum Oakley <c.oakley108@gmail.com>
2018-01-11 21:54:31 +0000

    052

---
+++ problem_052.py
@@ -0,0 +1,11 @@
+def infRange(n=0, step=1):
+    while True:
+        yield n
+        n += step
+
+def search(multiples):
+    for x in infRange(1):
+        if len({ tuple(sorted(str(k * x))) for k in multiples }) == 1:
+            return x
+
+print(search([1, 2, 3, 4, 5, 6]))
