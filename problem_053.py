20d920291b1eb3868d030777f18bcd1fc5833194
Callum Oakley <c.oakley108@gmail.com>
2018-01-12 13:39:57 +0000

    .

--- problem_053.py
+++ problem_053.py
@@ -3,6 +3,4 @@ from math import factorial
 def choose(n, r):
     return factorial(n) // (factorial(r) * factorial(n - r))
 
-print(sum(
-    1 for n in range(1, 101) for r in range(1, n + 1) if choose(n, r) > 1e6
-))
+print(sum(1 for n in range(101) for r in range(n + 1) if choose(n, r) > 1e6))
