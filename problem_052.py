77cf0737c17ba9337005b17ccfa4a6a76d12d9fa
Callum Oakley <c.oakley108@gmail.com>
2018-01-12 13:29:29 +0000

    .

--- problem_052.py
+++ problem_052.py
@@ -5,7 +5,7 @@ def infRange(n=0, step=1):
 
 def search(multiples):
     for x in infRange(1):
-        if len({ tuple(sorted(str(k * x))) for k in multiples }) == 1:
+        if len({tuple(sorted(str(k * x))) for k in multiples}) == 1:
             return x
 
 print(search([1, 2, 3, 4, 5, 6]))
--- problem_053.py
+++ problem_053.py
@@ -3,10 +3,6 @@ from math import factorial
 def choose(n, r):
     return factorial(n) // (factorial(r) * factorial(n - r))
 
-count = 0
-for n in range(1, 101):
-    for r in range(1, n + 1):
-        if choose(n, r) > 1e6:
-            count += 1
-
-print(count)
+print(sum(
+    1 for n in range(1, 101) for r in range(1, n + 1) if choose(n, r) > 1e6
+))
