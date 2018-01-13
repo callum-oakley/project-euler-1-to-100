a291f4c1a70ea072feac875e4748ef97a8382e2a
Callum Oakley <c.oakley108@gmail.com>
2018-01-13 00:42:47 +0000

    057

---
+++ problem_057.py
@@ -0,0 +1,9 @@
+from fractions import Fraction
+
+count = 0
+approx = Fraction(1)
+for n in range(1000):
+    approx = 1 + 1/(1 + approx)
+    if len(str(approx.numerator)) > len(str(approx.denominator)):
+        count += 1
+print(count)
