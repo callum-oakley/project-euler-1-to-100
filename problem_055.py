e08bf96b2c74e0527e6be25da72e310df54fb400
Callum Oakley <c.oakley108@gmail.com>
2018-01-13 00:22:15 +0000

    055

---
+++ problem_055.py
@@ -0,0 +1,11 @@
+def reverse(n):
+    return int("".join(reversed(str(n))))
+
+def isLychrel(n):
+    for _ in range(49):
+        n += reverse(n)
+        if n == reverse(n):
+            return False
+    return True
+
+print(sum(1 for n in range(10**4) if isLychrel(n)))
