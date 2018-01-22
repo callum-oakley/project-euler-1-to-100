a9072f41bf79333e007117083de0c207807fe730
Callum Oakley <c.oakley108@gmail.com>
2018-01-22 22:35:36 +0000

    076

---
+++ problem_076.py
@@ -0,0 +1,10 @@
+from math import inf
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def decompositions(n, max_part=inf):
+    return (0 if n > max_part else 1) + sum(
+        decompositions(n - m, m) for m in range(1, min(max_part + 1, n))
+    )
+
+print(decompositions(100) - 1) # includes the trivial decomposition
