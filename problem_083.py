01108c51822799767e11108cdacb6bd7b482feed
Callum Oakley <c.oakley108@gmail.com>
2018-08-06 23:00:40 +0100

    083

--- problem_083.py
+++ problem_083.py
@@ -6,28 +6,36 @@ def parse(file):
 # Dijkstra's algorithm specialised to the case of a grid with adjacent nodes
 # connected. Since we have weighted nodes rather than weighted edges, treat the
 # weight of an edge from A to B as the weight of B.
-def dijkstra(matrix, initial, target):
-    unvisited = {(x, y)
-                 for y in range(len(matrix)) for x in range(len(matrix[y]))}
-    distances = {initial: matrix[initial[1]][initial[0]]}
+def dijkstra(n, weight, neighbors, target):
+    unvisited = {(x, y) for y in range(n) for x in range(n)}
+    distances = {(0, 0): weight((0, 0))}
+
+    def d(v):
+        return distances[v]
 
     while target in unvisited:
-        (x, y) = min(
-            (node for node in unvisited if node in distances),
-            key=lambda node: distances[node])
+        current = min((v for v in unvisited if v in distances), key=d)
 
-        unvisited_neighbors = ((xp, yp) for (xp, yp) in
-                               ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1))
-                               if (xp, yp) in unvisited)
+        for v in (v for v in neighbors(current) if v in unvisited):
+            if v not in distances or d(current) + weight(v) < d(v):
+                distances[v] = d(current) + weight(v)
 
-        for (xp, yp) in unvisited_neighbors:
-            if (xp, yp) not in distances or distances[(
-                    x, y)] + matrix[yp][xp] < distances[(xp, yp)]:
-                distances[(xp, yp)] = distances[(x, y)] + matrix[yp][xp]
+        unvisited.remove(current)
 
-        unvisited.remove((x, y))
-
-    return distances[target]
+    return d(target)
 
 
-print(dijkstra(parse('data/083'), (0, 0), (79, 79)))
+matrix = parse('data/083')
+
+
+def neighbors(v):
+    x, y = v
+    return ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1))
+
+
+def weight(v):
+    x, y = v
+    return matrix[y][x]
+
+
+print(dijkstra(80, weight, neighbors, (79, 79)))
