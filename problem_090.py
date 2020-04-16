22b4b63417a7dd121060fcc08e6c7075d0c93a13
Callum Oakley <c.oakley108@gmail.com>
2020-04-16 19:51:37 +0100

    090

---
+++ problem_090.py
@@ -0,0 +1,34 @@
+def subsets(xs, length):
+    if length == 0:
+        yield set()
+    elif len(xs) == length:
+        yield xs
+    else:
+        xs = xs.copy()
+        x = xs.pop()
+        yield from ({x} | ys for ys in subsets(xs, length - 1))
+        yield from subsets(xs, length)
+
+
+targets = [
+    (6 if tens == 9 else tens, 6 if units == 9 else units)
+    for tens, units in (divmod(n ** 2, 10) for n in range(1, 10))
+]
+
+
+def valid(dice1, dice2):
+    return all(
+        tens in dice1 and units in dice2 or tens in dice2 and units in dice1
+        for tens, units in targets
+    )
+
+
+dice = [{6 if f == 9 else f for f in d} for d in subsets(set(range(10)), 6)]
+
+count = 0
+for i in range(len(dice)):
+    for j in range(i + 1):
+        if valid(dice[i], dice[j]):
+            count += 1
+
+print(count)
