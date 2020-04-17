b488238efbc359a58a03b6b08183e02677ea9b36
Callum Oakley <c.oakley108@gmail.com>
2020-04-17 18:30:22 +0100

    092

---
+++ problem_092.py
@@ -0,0 +1,20 @@
+from functools import lru_cache
+
+
+@lru_cache(maxsize=None)
+def square_digit_sum(n):
+    if n < 10:
+        return n ** 2
+    return square_digit_sum(int(str(n)[::2])) + square_digit_sum(
+        int(str(n)[1::2])
+    )
+
+
+@lru_cache(maxsize=None)
+def destination(n):
+    if n == 1 or n == 89:
+        return n
+    return destination(square_digit_sum(n))
+
+
+print(len([n for n in range(1, 10 ** 7) if destination(n) == 89]))
