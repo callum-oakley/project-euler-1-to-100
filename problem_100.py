3972d11328048ed208c6c89a14241f2010709bf5
Callum Oakley <c.oakley108@gmail.com>
2020-05-13 14:58:19 +0100

    wip

---
+++ problem_100.py
@@ -0,0 +1,21 @@
+from math import sqrt
+
+
+# generating the first few values in the sequence naively leads us to
+# https://oeis.org/A011900
+# TODO prove why this is the case
+def blue(a, b):
+    while True:
+        a, b = b, 6 * b - a - 2
+        yield b
+
+
+def search():
+    for b in blue(1, 3):
+        # TODO this is a hack, calculate total exactly
+        total_ish = sqrt(2) * b
+        if total_ish > 10 ** 12:
+            return b
+
+
+print(search())
